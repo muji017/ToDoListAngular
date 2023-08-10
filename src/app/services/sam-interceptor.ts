c0416a4dcd7484af8dd51839d5d0fc541051a2f1
mujeeb <mujeebrahmanps01707@gmail.com>
2023-08-10 20:40:06 +0530

    Set-ApiUrl-Interceptor

--- src/app/services/sam-interceptor.ts
+++ src/app/services/sam-interceptor.ts
@@ -2,13 +2,19 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/c
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-@Injectable() export class SamInterceptor implements HttpInterceptor{
+@Injectable()
+ export class SamInterceptor implements HttpInterceptor{
+
+    private baseUrl='http://localhost:5000'
  
      intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      
+    
+
         const modReq=req.clone({
+            url:`${this.baseUrl}${req.url}`,
             setHeaders:{
-                'content-type':'Application/json'},
+                'content-type':'Application/json'
+            },
         })
         return next.handle(modReq)
      }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,36 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http'
-import { SamInterceptor } from './sam-interceptor';
+// import { SamInterceptor } from './sam-interceptor';
 import {Observable} from 'rxjs'
 import { Task } from '../Task';
-
-// const httpOptions={
-//   headers:new HttpHeaders({
-//     'Content-Type':'Application/json'
-//   })
-// }
-
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl='http://localhost:5000/tasks'
   constructor(private http:HttpClient) { }
   
+  private endPoint='/tasks'
+
   getTask():Observable<Task[]>{
-   return this.http.get<Task[]>(this.apiUrl)
+   return this.http.get<Task[]>(this.endPoint)
   }
   deleteTask(task:Task):Observable<Task>{
-    const url=`${this.apiUrl}/${task.id}`
+    const url=`${this.endPoint}/${task.id}`
     return this.http.delete<Task>(url)
   }
   updateReminder(task:Task):Observable<Task>{
-    const url=`${this.apiUrl}/${task.id}`
+    const url=`${this.endPoint}/${task.id}`
     return this.http.put<Task>(url,task)
   }
   addTask(task:Task):Observable<Task>{
-    return this.http.post<Task>(this.apiUrl,task)
+    return this.http.post<Task>(this.endPoint,task)
   }
 }
